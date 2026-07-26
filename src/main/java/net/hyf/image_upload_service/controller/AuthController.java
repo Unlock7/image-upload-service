1b89981bde334f27c99b1615fcacf04942a5fbcc
Dagim02 <dagimvanhaileselassie@gmail.com>
2026-07-26 13:25:11 +0200

    feat: implement user registration endpoint

--- src/main/java/net/hyf/image_upload_service/controller/AuthController.java
+++ src/main/java/net/hyf/image_upload_service/controller/AuthController.java
@@ -1,4 +1,32 @@
 package net.hyf.image_upload_service.controller;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import net.hyf.image_upload_service.dto.auth.RegisterRequest;
+import net.hyf.image_upload_service.dto.auth.UserResponse;
+import net.hyf.image_upload_service.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(
+            @Valid @RequestBody RegisterRequest request
+    ) {
+        UserResponse response = authService.register(request);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
 }
