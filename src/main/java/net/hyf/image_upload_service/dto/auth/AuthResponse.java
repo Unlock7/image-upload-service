680629889743ed370fda3cb349922a0415293480
Dagim02 <dagimvanhaileselassie@gmail.com>
2026-07-26 17:52:15 +0200

    feat: add login endpoint with session cookie

--- src/main/java/net/hyf/image_upload_service/dto/auth/AuthResponse.java
+++ src/main/java/net/hyf/image_upload_service/dto/auth/AuthResponse.java
@@ -1,4 +1,14 @@
 package net.hyf.image_upload_service.dto.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.Instant;
+
+@Getter
+@AllArgsConstructor
 public class AuthResponse {
+
+    private String message;
+    private Instant expiresAt;
 }
